eaac4addef45b0e425ca0e95d3a60a8228a54065
fathimashifaak <fathimashifa898@gmail.com>
2025-04-15 11:54:05 +0530

    Add files via upload

---
+++ time.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hour=$(date +"%H")
+
+if (( hour < 12 )); then
+  echo "Good Morning!"
+elif (( hour < 17 )); then
+  echo "Good Afternoon!"
+elif (( hour < 20 )); then
+  echo "Good Evening!"
+else
+  echo "Good Night!"
+fi
+
